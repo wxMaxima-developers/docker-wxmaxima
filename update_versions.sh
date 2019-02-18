16d05becaad703d98879d7b2e7d3813247c08e93
Gunter Königsmann <gunter@peterpall.de>
2019-02-18 06:52:44 +0100

    Updated the version number of wxMaxima

--- update_versions.sh
+++ update_versions.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 maxima_build='tags\/5.42.2'
-wxmaxima_build='Version-19.02.0'
+wxmaxima_build='Version-19.02.1'
 
 sed -i -e "s/ENV maxima_build .*/ENV maxima_build $maxima_build/" Dockerfile
 sed -i -e "s/ENV wxmaxima_build .*/ENV wxmaxima_build $wxmaxima_build/" Dockerfile
