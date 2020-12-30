339302008a5dacdce465ee2511eca334146dc776
Gunter Königsmann <gunter@peterpall.de>
2020-12-30 18:44:18 +0100

    Updated the version number

--- update_versions.sh
+++ update_versions.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 maxima_build='tags\/5.44.0'
-wxmaxima_build='Version-20.12.1'
+wxmaxima_build='Version-20.12.2'
 
 sed -i -e "s/ENV maxima_build .*/ENV maxima_build $maxima_build/" Dockerfile
 sed -i -e "s/ENV wxmaxima_build .*/ENV wxmaxima_build $wxmaxima_build/" Dockerfile
