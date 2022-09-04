fc8a3fb34982ef11224768d9e387c8cf9b68871c
Gunter Königsmann <gunter@peterpall.de>
2022-09-04 11:19:37 +0200

    All that I have learned from trying to build a flatpack

---
+++ build_flatpack.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+ENV wxmaxima_build Version-22.05.0
+
+export wxmaxima_build=Version-22.05.0
+export maxima_build=tags/5.46.0
+
+git clone https://github.com/wxMaxima-developers/wxmaxima.git
+cd wxmaxima
+git checkout ${wxmaxima_build}
+cd ..
+
+git clone https://git.code.sf.net/p/maxima/code maxima-code
+cd maxima-code
+git checkout ${maxima_build}
+cd ..
+
+mkdir flatpack
+flatpack-builder flatpack com.github.wxMaxima-developers.wxmaxima.yml
--- update_versions.sh
+++ update_versions.sh
@@ -5,3 +5,5 @@ wxmaxima_build='Version-22.09.0'
 
 sed -i -e "s/ENV maxima_build .*/ENV maxima_build $maxima_build/" Dockerfile
 sed -i -e "s/ENV wxmaxima_build .*/ENV wxmaxima_build $wxmaxima_build/" Dockerfile
+sed -i -e "s/export wxmaxima_build.*/export wxmaxima_build=$wxmaxima_build/" build_flatpack.sh
+sed -i -e "s/export maxima_build.*/export maxima_build=$maxima_build/" build_flatpack.sh
