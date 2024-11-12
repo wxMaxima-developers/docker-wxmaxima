58d3aa74b83cd86486d2b8894459b7c6501b6c23
Wolfgang Dautermann <dauti@users.sourceforge.net>
2024-11-13 00:48:38 +0100

    Update versions.

--- update_versions.sh
+++ update_versions.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-maxima_build='tags\/5.45.1'
-wxmaxima_build='Version-21.11.0'
+maxima_build='tags\/5.47.0'
+wxmaxima_build='Version-24.11.0'
 
 sed -i -e "s/ENV maxima_build .*/ENV maxima_build $maxima_build/" Dockerfile
 sed -i -e "s/ENV wxmaxima_build .*/ENV wxmaxima_build $wxmaxima_build/" Dockerfile
